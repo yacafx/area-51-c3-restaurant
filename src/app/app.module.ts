fdf52c7d82daf3cb0067ff0af43d877d27a6a9dc
yacaFx <sergiobritor@gmail.com>
2021-04-10 13:32:52 -0400

    feat(general): add dish & menu components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DishComponent } from './dish/dish.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DishComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/dish/dish.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rw-dish',
+  templateUrl: './dish.component.html',
+  styleUrls: ['./dish.component.scss']
+})
+export class DishComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
