190933e0f5cbd5dc901110b3a8cd82c84d7f56a9
yacaFx <sergiobritor@gmail.com>
2021-04-10 13:53:04 -0400

    feat(general): add ingirdients component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { DishComponent } from './dish/dish.component';
 import { MenuComponent } from './menu/menu.component';
+import { IngredientsComponent } from './ingredients/ingredients.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DishComponent,
-    MenuComponent
+    MenuComponent,
+    IngredientsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/ingredients/ingredients.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rw-ingredients',
+  templateUrl: './ingredients.component.html',
+  styleUrls: ['./ingredients.component.scss']
+})
+export class IngredientsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
