d737b9f103433d35ab912d7758f8e1dd58dd9932
yacaFx <sergiobritor@gmail.com>
2021-04-17 14:39:48 -0400

    feat(session-3): display multiple dishes with ngFor

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Dish } from 'projects/data-models/src/public-api';
 
 @Component({
   selector: 'rw-menu',
@@ -6,7 +7,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent implements OnInit {
+  dishes: Dish[] = [];
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.dishes = [...this.initDishes()];
+  }
+
+  initDishes(): Dish[] {
+    const dishes: Dish[] = [
+      {
+        name: 'Ceviche',
+        price: '1234',
+      },
+      {
+        name: 'Pollo a la brasa',
+        photo:
+          'https://www.radionacional.com.pe/sites/default/files/styles/note/public/pollo_brasa.jpg',
+        price: '78383',
+      },
+      {
+        name: 'Causa rellena',
+        price: '8762',
+      },
+      {
+        name: 'Tallarines Rojos',
+        price: '2837',
+      },
+      {
+        name: 'Lomo Saltado',
+        photo:
+          'https://www.196flavors.com/wp-content/uploads/2016/07/lomo-saltado-1-FP.jpg',
+        price: '13434',
+      },
+      {
+        name: 'Arroz con pato',
+        photo:
+          'https://lascomidasperuanas.com/wp-content/uploads/2020/06/Receta-de-arroz-con-pato-peruano.jpg',
+        price: '3457',
+      },
+      {
+        name: 'Chanfainita',
+        photo:
+          'https://decomidaperuana.com/wp-content/uploads/2018/06/receta-de-chanfainita.jpg',
+        price: '8457',
+      },
+    ];
+    return dishes;
+  }
 }
