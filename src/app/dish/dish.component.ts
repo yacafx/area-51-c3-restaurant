c024aa0fd76effa18761a4fa5b3f5ac93602eeb1
yacaFx <sergiobritor@gmail.com>
2021-04-17 11:47:36 -0400

    feat(session-3): property binding

--- src/app/dish/dish.component.ts
+++ src/app/dish/dish.component.ts
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dish.component.scss'],
 })
 export class DishComponent implements OnInit {
-  constructor() {}
+  name: string;
+  placeholder: string;
+  photo: string;
+
+  constructor() {
+    this.placeholder = 'El platillo es...';
+    this.name = '';
+    // this.photo =
+    //   'https://www.diabelife.com/wp-content/uploads/2015/08/pan-integral-con-palta-y-huevo.jpg';
+    this.photo = 'assets/dish-1.jpeg';
+  }
 
   ngOnInit(): void {}
 }
