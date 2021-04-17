a9b07b4e5a19dd10552d057140160db3d7b929ae
yacaFx <sergiobritor@gmail.com>
2021-04-17 12:36:05 -0400

    feat(session-3): event binding

--- src/app/dish/dish.component.ts
+++ src/app/dish/dish.component.ts
@@ -12,11 +12,30 @@ export class DishComponent implements OnInit {
 
   constructor() {
     this.placeholder = 'El platillo es...';
-    this.name = '';
+    this.name = 'Pan con palta';
     // this.photo =
     //   'https://www.diabelife.com/wp-content/uploads/2015/08/pan-integral-con-palta-y-huevo.jpg';
     this.photo = 'assets/dish-1.jpeg';
   }
 
   ngOnInit(): void {}
+
+  changeName(): void {
+    this.name = 'Pan con huevo';
+  }
+
+  handleInput(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    console.log('Input -> ' + input.value);
+  }
+
+  handleBlur(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    console.log('Blur -> ' + input.value);
+  }
+
+  handleKeyUp(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    console.log('KeyUp -> ' + input.value);
+  }
 }
