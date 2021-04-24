9b0728bb5627067806f55b76e1ba83853e27b336
yacaFx <sergiobritor@gmail.com>
2021-04-24 14:50:05 -0400

    feat(session-4): font awesome integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
 import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [AppComponent, MenuComponent],
-  imports: [BrowserModule, ComponentsModule, FormsModule],
+  imports: [BrowserModule, ComponentsModule, FormsModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,11 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import {
+  FaIconLibrary,
+  FontAwesomeModule,
+} from '@fortawesome/angular-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { DishComponent } from './dish/dish.component';
 import { IngredientsComponent } from './ingredients/ingredients.component';
-
 @NgModule({
   declarations: [DishComponent, IngredientsComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   exports: [DishComponent, IngredientsComponent],
 })
-export class ComponentsModule {}
+export class ComponentsModule {
+  constructor(private library: FaIconLibrary) {
+    library.addIcons(faPlus);
+  }
+}
