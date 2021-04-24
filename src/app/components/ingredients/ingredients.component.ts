47daccb2cf0bb87cf6903242dba95b6a3fd6c77c
yacaFx <sergiobritor@gmail.com>
2021-04-24 15:49:08 -0400

    feat(session-4):  use a simple async/await

--- src/app/components/ingredients/ingredients.component.ts
+++ src/app/components/ingredients/ingredients.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from 'projects/data-models/src/lib/interfaces/country.interfaces';
 
 @Component({
   selector: 'rw-ingredients',
   templateUrl: './ingredients.component.html',
-  styleUrls: ['./ingredients.component.scss']
+  styleUrls: ['./ingredients.component.scss'],
 })
 export class IngredientsComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.loadCountries().then((country) => console.log(country));
   }
 
+  async loadCountries(): Promise<Country> {
+    const response = await fetch('https://restcountries.eu/rest/v2/all');
+
+    const countries = await response.json();
+
+    return countries.filter((country: Country) => country.name === 'Peru')[0];
+  }
 }
