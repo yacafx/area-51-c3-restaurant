13faff12fe4546c7d7d9b50e3fc7f21f3eda076b
yacaFx <sergiobritor@gmail.com>
2021-04-24 16:02:00 -0400

    feat(session-4): async/await with parallel requests

--- src/app/components/ingredients/ingredients.component.ts
+++ src/app/components/ingredients/ingredients.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Country } from 'projects/data-models/src/lib/interfaces/country.interfaces';
+import { Person } from 'projects/data-models/src/lib/interfaces/people.interfaces';
 
 @Component({
   selector: 'rw-ingredients',
@@ -10,14 +11,24 @@ export class IngredientsComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    this.loadCountries().then((country) => console.log(country));
+    this.loadCountries().then((result) => console.log(result));
   }
 
-  async loadCountries(): Promise<Country> {
-    const response = await fetch('https://restcountries.eu/rest/v2/all');
+  async loadCountries(): Promise<[Country, Person[]]> {
+    const [responseCountries, responseSwars] = await Promise.all([
+      fetch('https://restcountries.eu/rest/v2/all'),
+      fetch('https://swapi.dev/api/people'),
+    ]);
 
-    const countries = await response.json();
+    const countries = await responseCountries.json();
+    const responseSWapi = await responseSwars.json();
 
-    return countries.filter((country: Country) => country.name === 'Peru')[0];
+    const country: Country = countries.filter(
+      (item: Country) => item.name === 'Peru'
+    )[0];
+
+    const people: Person[] = responseSWapi.results;
+
+    return [country, people];
   }
 }
