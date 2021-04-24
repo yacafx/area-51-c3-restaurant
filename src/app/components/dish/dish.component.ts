95a7c4219a30446011523e9a6026ccfb53220532
yacaFx <sergiobritor@gmail.com>
2021-04-24 13:24:24 -0400

    feat(session-4): pass input properties to a stateless coomponent

--- src/app/components/dish/dish.component.ts
+++ src/app/components/dish/dish.component.ts
@@ -1,17 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'rw-dish',
   templateUrl: './dish.component.html',
   styleUrls: ['./dish.component.scss'],
 })
-export class DishComponent implements OnInit {
-  photo = 'https://via.placeholder.com/300.png/EFF1FA/3850b7?text=The+Dishes';
-  name = 'Default name';
-  price = 0;
-  stock = false;
+export class DishComponent implements OnInit, OnChanges {
+  @Input() photo: string | undefined;
+  @Input() name = 'Default name';
+  @Input() price: number | undefined;
+  @Input() stock: boolean | undefined;
+
+  private placeholder =
+    'https://via.placeholder.com/300.png/EFF1FA/3850b7?text=The+Dishes';
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.photo = changes.photo.currentValue || this.placeholder;
+  }
 }
